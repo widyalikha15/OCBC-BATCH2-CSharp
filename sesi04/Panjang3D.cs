1f12257fe9b3279b18d454a803aa0df6b194d44a
Nama <widyalikha.15@gmail.com>
2021-11-23 15:34:22 +0700

    ss

--- sesi04/Panjang3D.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-class Panjang3D {
-    static void Main(){
-        int[, ,] angka = new int[10, 5, 6];
-
-        Console.WriteLine("Panjang array angka adalah " + angka.Length);
-    }
-}
