c15975d2cdaa8b6f7e5be20d59125a4eb26fbefd
Nama <widyalikha.15@gmail.com>
2021-11-30 10:33:36 +0700

    update unit test mock

---
+++ sesi12/UnitTest_Mock/UnitTest_Mock/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnitTest_Mock.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Designation { get; set; }
+    }
+}
